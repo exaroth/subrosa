0254e69df76526fd7537adf86adccc796bb0757a
Konrad Wąsowicz <exaroth@gmail.com>
2014-03-04 18:31:56 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "Subrosa",
+    version = "0.3",
+    long_description = __doc__,
+    packages=["main"]
+    include_package_data = True,
+    zip_safe = False,
+    install_requires=["Flask>=10.1",
+                      "Flask-Cache",
+                      "Markdown",
+                      "Pygments",
+                      "requests",
+                      "peewee",
+                      "pathlib"]
+
+)
