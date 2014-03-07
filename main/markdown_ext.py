41ca19d58bda382c330beef15b1df4528554616a
Konrad Wąsowicz <exaroth@gmail.com>
2014-03-07 18:38:30 +0100

    Applied consistent reindents

--- main/markdown_ext.py
+++ main/markdown_ext.py
@@ -3,14 +3,14 @@
 
     main.markdown_ext
     ============
- 	
- 	Implements class integrating markdown
- 	parser into Subrosa
 
- 	Inspired by Flask-Markdown extension
- 	by Dan Colish
+        Implements class integrating markdown
+        parser into Subrosa
 
- 	https://github.com/dcolish/flask-markdown
+        Inspired by Flask-Markdown extension
+        by Dan Colish
+
+        https://github.com/dcolish/flask-markdown
 
     :copyright: (c) 2014 by Konrad Wasowicz
     :license: MIT, see LICENSE for details.
@@ -25,33 +25,31 @@ import markdown
 
 class Markdown(object):
 
-	"""
+    """
 
-	Wrapper class for Markdown object
+    Wrapper class for Markdown object
 
-	Example usage:
-	md = Markdown(app = app,
-				  auto_escape = False,
-				  extensions = ['footnotes'],
-				  safe_mode = True,
-				  output_format = 'html4')
-	"""
+    Example usage:
+    md = Markdown(app = app,
+                              auto_escape = False,
+                              extensions = ['footnotes'],
+                              safe_mode = True,
+                              output_format = 'html4')
+    """
 
-	def __init__(self, app, auto_escape = False, **markdown_options):
+    def __init__(self, app, auto_escape = False, **markdown_options):
 
-		self._instance = markdown.Markdown(**markdown_options)
+        self._instance = markdown.Markdown(**markdown_options)
 
-	def __call__(self, stream):
-		return Markup(self._instance.convert(stream))
+    def __call__(self, stream):
+        return Markup(self._instance.convert(stream))
 
-	def _build_filter(self, auto_escape):
-		@evalcontextfilter
-		def markdown_filter(eval_ctx, stream):
-			_filter = self
-			if auto_escape and eval_ctx.autoescape:
-				return Markup(_filter(escape(stream)))
-			else:
-				return Markup(_filter(stream))
-		return markdown_filter
-
-
+    def _build_filter(self, auto_escape):
+        @evalcontextfilter
+        def markdown_filter(eval_ctx, stream):
+            _filter = self
+            if auto_escape and eval_ctx.autoescape:
+                return Markup(_filter(escape(stream)))
+            else:
+                return Markup(_filter(stream))
+        return markdown_filter
