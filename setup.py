01f7286ba9768ec2ef896d94d5e418b965998667
Konrad Wąsowicz <exaroth@gmail.com>
2014-04-10 22:42:49 +0200

    added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
-import multiprocessing
 
 setup(
     name = "Subrosa",
     version = "0.3",
     author = "Konrad Wasowicz",
     author_email = "exaroth@gmail.com",
-    description = "Simple and beatiful blogging system",
-    url = "subrosa.github.io",
+    description = "Simple and elegant blogging system",
+    url = "http://subrosa.readthedocs.org",
     license = "GPL v3",
     long_description = __doc__,
     packages=["subrosa"],
@@ -19,5 +18,17 @@ setup(
                       "six",
                       "Pygments",
                       "peewee"]
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python",
+        "Framework :: Flask",
+        "OSI Approved :: Gnu General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Communications",
+        "Topic :: WWW/HTTP"
+    ]
 
 )
